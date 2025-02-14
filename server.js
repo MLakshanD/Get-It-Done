ce033ec0bb0790fe94bca0465d22934e6c00a8ce
MLakshanD <56705128+MLakshanD@users.noreply.github.com>
2025-02-15 00:07:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors({
     origin: [
         'http://localhost:5173',  // Local Vite development server
-        'https://your-netlify-app.netlify.app', // Replace with your Netlify domain
+        'https://get-it-dn.netlify.app', // Replace with your Netlify domain
     ],
     methods: ['GET', 'POST'],
     credentials: true
